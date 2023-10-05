5fbfce823d5cd6f69b21f8801da875a3fc4d1554
LMHoang20 <leminhhoang7554@gmail.com>
2023-10-05 23:02:23 +0700

    add recovery middleware

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	"net/http"
 )
 
+func RecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil && err != http.ErrAbortHandler {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	if err := database.Instance(constants.DBType).Initialize(); err != nil {
 		fmt.Println(err)
@@ -17,7 +29,7 @@ func main() {
 	r := router.Init()
 
 	fmt.Println("Server running on port", constants.Port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", constants.Port), r); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", constants.Port), RecoveryMiddleware(r)); err != nil {
 		fmt.Println(err)
 	}
 }
